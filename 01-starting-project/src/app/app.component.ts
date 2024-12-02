e97f06d1a84759eeb7c7949bc919cb2f72f7bea6
TOFIK0143 <tofikmaniyar0143@gmail.com>
2024-12-02 15:20:58 +0530

    Changes made on 02/12/2024-55

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from "./tasks/tasks.component";
+import { TaskComponent } from './tasks/task/task.component';
 
 @Component({
     selector: 'app-root',
